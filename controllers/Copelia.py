b43a4f43728cfb27b2df606cc0caad951472ca85
pierresousa <pierrevssousa@gmail.com>
2022-07-05 21:30:33 -0300

    telemetria copelia

--- controllers/Copelia.py
+++ controllers/Copelia.py
@@ -1,11 +1,27 @@
 from controllers.Strategy import Strategy
 from controllers.Crane_lite import Crane_Lite
 from numpy import power
+import globalData
+import threading
+import time
+
 class Copelia(Strategy):
     Model = None
     
     def __init__(self):
         self.Model = Crane_Lite()
+        existsThread = next((thread for thread in threading.enumerate() if thread.name == 'thread_atualiza_telemetria_copelia'), False)
+        if existsThread == False:
+            updateTelemetriaThread = threading.Thread(target = self.thread_atualiza_telemetria, daemon=True, name="thread_atualiza_telemetria_copelia")
+            updateTelemetriaThread.start()
+
+    def thread_atualiza_telemetria(self):
+        valores = self.valor_sensores()
+        globalData.distanceTool = int(valores['distanceTool'])
+        globalData.towerPosition = int(valores['towerPosition'])
+        globalData.electromagnet = int(valores['electromagnet'])
+        globalData.toolPosition = int(valores['toolPosition'])
+        time.sleep(1)
         
     def rotacionar_torre(self, graus: int) -> bool:
         self.Model.XY.set_Position(self.Model.XY.getPosition()+graus)
